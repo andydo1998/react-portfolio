f0b6d459ca50d3d1d970f7f7939c83036fcca21d
andydo1998 <37276395+andydo1998@users.noreply.github.com>
2022-06-20 18:38:43 -0700

    Finished all sections, todo: fix meme, add navbar to header, add social media to footer

--- src/Apps/Tenzies.js
+++ src/Apps/Tenzies.js
@@ -6,7 +6,7 @@ import Confetti from "react-confetti";
 
 export default function Tenzies() {
     // variable to change the number of die to appear (default: 10)
-    const NUMBER_OF_DIE = 10;
+    const NUMBER_OF_DIE = 2;
 
     // state for all dice
     const [dice, setDice] = React.useState(allNewDice());
@@ -83,7 +83,7 @@ export default function Tenzies() {
     // function used to display winning message
     function winningMessage() {
         alert(`You won in ${numRoll} rolls!`);
-        return <Confetti />;
+        return <Confetti height={5000} gravity={2} />;
     }
 
     // function used to reset the game (called once the user has won)
--- src/Components/Section_4.js
+++ src/Components/Section_4.js
@@ -4,8 +4,20 @@ import Tenzies from "../Apps/Tenzies";
 
 export default function Section_4() {
     return (
-        <div>
-            <Tenzies />
+        <div className="section_4--container">
+            <div className="section_4--description-container">
+                <p className="section_4--description">
+                    If you got here, I want to thank you so much 
+                    for giving my little portfolio a read! There is still so much more for me to
+                    learn, as well as numerous ways to improve my existing code! These past 3 weeks have been
+                    an adventure to learn React and I hope that this little portfolio has shown you the amount
+                    of content I have learned in the past weeks. To finish things off, here's this little Tenzies game
+                    that I made!
+                </p>
+            </div>
+            <div className="tenzies--container">
+                <Tenzies />
+            </div>
         </div>
     )
 }
